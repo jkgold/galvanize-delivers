4c99cb03a27c08877b8ae5102ad940ae6aad895d
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-10-23 11:56:24 -0600

    got Jason to append to tbody

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $('.parallax').parallax();
+    // $('a').on('click', function() {
+    // console.log("Yo mama's got a glass eye with a fish in it!");
+    $("a").on('click',function(event) {
+  var $target = $(event.target);
+var $test = 'Jason';
+// var $oppCheck = $target.attr("class");
+$("tbody").append($test);
   });
 
-console.log("World hello");
+  });
