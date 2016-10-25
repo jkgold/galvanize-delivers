e1a38bfcd2cf37a2683d18748d1d90d6c0c29f95
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-10-24 18:14:15 -0600

    adding names and price works

--- app.js
+++ app.js
@@ -1,22 +1,62 @@
 $(document).ready(function(){
     $('.parallax').parallax();
-    // $('a').on('click', function() {
-    // console.log("Yo mama's got a glass eye with a fish in it!");
-    $("a").on('click',function(event) {
-  var $target = $(event.target);
-var $test = 'Jason';
-var $addToOrder = $target.attr(".card-content")
-var $oppCheck = $target.attr("class");
-$("#sub").append($test);
-$('a').on('click',(function(event) {
-   $('#myTable tbody').append('<tr class=".card-content"><td>"name", "price"</td></tr>');
-});
 
-  });
+  })
+  var
+    function addToOrder (name, price){
+      $('<tr><td>' + name + '</td><td></td>' +'<td>' + "$"+ price + '</td></tr>').insertBefore('#sub');
+    // addToOrder();
+  }
+
+  //   $( document ).ready(function() {
+
+  // $("a").on('click',function(event) {
+  //
+  //   var $newTr = document.createElement("td");
+  //   console.log($newTr);
+  //   var $newTr = document.createElement("tr")
+
+    // var $target = $(event.target);
+    // // var $test = 'Jason';
+    // var $addToOrder = $target.attr("class",".card-content");
+    // // // var $oppCheck = $target.attr("");
+    // $("#sub").append($addToOrder);
+  // });
+
+
+/******
+you are adding the literal string of the a link
+
+try targeting the card and adding the text of the card
+
+
+*******/
+
+
+// }
 
   // });
-  function addToOrder (name, price){
-    td.text(name);
-    td.text(price)
+
   // });
-  });
+
+
+
+
+
+
+  // var target = $("event.target");
+  // var buttons = $(".order-button");
+  // $(buttons).on("click", function(){
+  //   if ($(target.attr("id") === 'burger')) {
+  //     console.log("burger");
+  //   } else if ($(target.attr("id") === 'pizza')) {
+  //     console.log("pizza");
+  //   } else if ($(target.attr("id") === 'ribs')) {
+  //     console.log("ribs");
+  //   } else {
+  //     console.log("ice cream");
+  //   }
+  // })
+
+  // $('a').on('click', function() {
+  // console.log("Yo mama's got a glass eye with a fish in it!");
